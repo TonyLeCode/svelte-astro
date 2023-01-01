36b16ba4b989fbe62d736ee3fd0d101149424ef0
TonyLeCode~ <103531472+TonyLeCode@users.noreply.github.com>
2022-12-31 16:45:41 -0800

    initial commit

---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let message
+  export let title
+</script>
+
+<div class="card">
+  <h2>{title}</h2>
+  {@html message}
+  <hr>
+</div>
+
+<style>
+  .card {
+    margin-bottom: 5rem;
+    
+  }
+
+  hr {
+    margin-top: 2rem;
+  }
+  
+</style>
